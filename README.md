272a35649fd77d11354ef1a245fd95ae97db19ed
Victor Hernandes Silva Maia <srvictormaia@gmail.com>
2015-12-23 15:40:57 -0200

    Improved examples and README.

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 # Reflex Screen Widget
 
-A Canvas-based widget that takes a dynamic, CPU-rendered image from Haskell (encoded as a ByteString), and renders it in realtime (using `onRequestAnimationFrame`).
+A Reflex widget that displays a dynamic Haskell image on the screen using `Canvas` and `onRequestAnimationFrame`.
 
+![Example gif](example.gif)
+
+---
+
+## Usage
+
+This code creates a constant 128x128 blue image signal and renders it to a
+canvas using `screenWidget`.
+
+```haskell
+import Reflex.Dom (mainWidget, constant, MonadWidget)
+import Reflex.Dom.Widget.Screen (screenWidget, ByteImageRgba(ByteImageRgba))
+import qualified Data.ByteString as BS
+
+blueScreen :: forall t m . MonadWidget t m => m ()
+blueScreen = do
+    let width  = 128
+    let height = 128
+    let blue   = [0, 0, 255, 255] -- [Red, Green, Blue, Alpha]
+    let buffer = BS.pack $ concat $ replicate (width*height) blue
+    let image  = ByteImageRgba width height buffer
+    screenCanvas <- screenWidget (constant image)
+    return ()
+```
+
+You can see the code for this and the other 2 examples above [on this file](https://github.com/maiavictor/ReflexScreenWidget/src/Main.hs). The resulting HTML/JS is [available live here](http://maiavictor.github.io/ReflexScreenWidget/).
--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,32 @@
-import Data.Time.Clock
-import Reflex.Dom.Widget.Screen.Test (screenWidgetTestApp)
-import Reflex.Dom (mainWidget)
+{-# LANGUAGE ScopedTypeVariables #-}
 
--- This will continuously render some images on JavaScript, using the CPU.
+import Data.Time.Clock
+import Reflex.Dom (mainWidget, constant, MonadWidget)
+import Reflex.Dom.Widget.Screen (screenWidget, ByteImageRgba(ByteImageRgba))
+import Reflex.Dom.Widget.Screen.Test (screenWidgetTestApp)
+import qualified Data.ByteString as BS
+
+-- The screen widget renders a ByteImageRgba dynamically.
+-- To create a ByteImageRgba, we need a width, a height,
+-- and a ByteString buffer with its color information, like
+-- this: `B.pack [R,G,B,A, R,G,B,A, ...]`. That is, the
+-- length of the buffer is `width * height * 4`.
+
+-- This is a 128x128 screen widget that is always blue:
+blueScreen :: forall t m . MonadWidget t m => m ()
+blueScreen = do
+    let width  = 128
+    let height = 128
+    let blue   = [0, 0, 255, 255] -- [Red, Green, Blue, Alpha]
+    let buffer = BS.pack $ concat $ replicate (width*height) blue
+    let image  = ByteImageRgba width height buffer
+    screenCanvas <- screenWidget (constant image)
+    return ()
+
+-- Starts an widget with 3 screens.
 main = do
     startTime <- getCurrentTime
     mainWidget $ do
+        blueScreen
         screenWidgetTestApp startTime True  128 128
         screenWidgetTestApp startTime False 128 128
